8a3f264662321890e4bab02bd6f142ab1af6e125
FIRST_Imad LAST_Kajdouh <2058092@talnet.nl>
2019-11-28 10:40:56 +0100

    Uitwerking van de exercise

---
+++ tijd.php
@@ -0,0 +1,21 @@
+<?PHP
+
+$v= readline( );
+$a = strpos($v,"s" );
+if ($a) {
+  
+
+    echo str_replace("s"," seconden", $v );    # code...
+}
+
+
+
+ else  {
+   
+    
+
+echo "geen tijd";
+}
+
+
+?>
